df7dc702f9eb9f95718b4888ae5b020542d035fe
Abhinav-22 <abhinavanil9@gmail.com>
2023-01-04 20:48:13 +0530

    Dependency resolved

---
+++ vsat/src/Individual.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Induvidual() {
+  return (
+    <>
+      <div className="App">
+        <h1 className="Heading">Induvidual Users</h1>
+
+        <div className="wrapper">
+          <input
+            type="text"
+            className="textbox"
+            id="outlined-basic"
+            label="YouTube Link"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Induvidual;
