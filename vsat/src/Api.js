51d56087a3b8615f33afee3e0601fe5d11d6bbf8
Abhinav-22 <abhinavanil9@gmail.com>
2023-03-22 07:33:10 +0530

    api bug

--- vsat/src/Api.js
+++ vsat/src/Api.js
@@ -120,8 +120,7 @@ const Api = () => {
                 </li>
                 <li>
                   <Link
-                    to="api"
-                
+                    to=""
                     className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-900"
                   >
                     <svg
