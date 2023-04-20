c2cd4d254d012e390648a404b6e50b7e1bacd6e0
Dijo Daiju <dijodaiju@pop-os.localdomain>
2023-04-20 13:42:39 +0530

    fixed dark mode

--- vsat/src/Api.js
+++ vsat/src/Api.js
@@ -295,7 +295,7 @@ const Api = () => {
                 <div className="relative">
                   <p
                     id="token"
-                    className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="block w-full p-4 pl-10 text-s rounded-lg   focus:border-blue-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 "
                     required
                   >
                     {tokenStore}{" "}
