77cdf37d6c3f676140a6c8b23d55d93b721f839c
Abhinav-22 <abhinavanil9@gmail.com>
2023-04-25 20:07:35 +0530

    added SSL expiry

--- vsat/src/Domain.js
+++ vsat/src/Domain.js
@@ -9,10 +9,21 @@ const Domain = () => {
   const [authName, setAuthName] = useState("");
   const [authEmail, setAuthEmail] = useState("");
   const [authCompany, setAuthCompany] = useState("");
+  const [validSSL, setValidSSL] = useState("Loading...");
 
   useEffect(() => {
     console.log("workingggg");
+    const updateSSL = async () => {
+      fetch("/sslexpiry")
+        .then((res) => res.json())
+        .then((data) => {
+          setValidSSL(data.SSLExpiry);
+          console.log(data);
+          console.log(data.SSLExpiry);
+        });
+    };
 
+    updateSSL();
     const fetchDetails = async () => {
       // await delay(1000);
 
@@ -281,11 +292,11 @@ const Domain = () => {
                 <path d="m10.273 2.513-.921-.944.715-.698.622.637.89-.011a2.89 2.89 0 0 1 2.924 2.924l-.01.89.636.622a2.89 2.89 0 0 1 0 4.134l-.637.622.011.89a2.89 2.89 0 0 1-2.924 2.924l-.89-.01-.622.636a2.89 2.89 0 0 1-4.134 0l-.622-.637-.89.011a2.89 2.89 0 0 1-2.924-2.924l.01-.89-.636-.622a2.89 2.89 0 0 1 0-4.134l.637-.622-.011-.89a2.89 2.89 0 0 1 2.924-2.924l.89.01.622-.636a2.89 2.89 0 0 1 4.134 0l-.715.698a1.89 1.89 0 0 0-2.704 0l-.92.944-1.32-.016a1.89 1.89 0 0 0-1.911 1.912l.016 1.318-.944.921a1.89 1.89 0 0 0 0 2.704l.944.92-.016 1.32a1.89 1.89 0 0 0 1.912 1.911l1.318-.016.921.944a1.89 1.89 0 0 0 2.704 0l.92-.944 1.32.016a1.89 1.89 0 0 0 1.911-1.912l-.016-1.318.944-.921a1.89 1.89 0 0 0 0-2.704l-.944-.92.016-1.32a1.89 1.89 0 0 0-1.912-1.911l-1.318.016z" />
               </svg>
               <span className="user font-medium text-lg ml-2 text-white">
-                SSL expiry in
+                SSL expires on
               </span>
               <br />
               <span className="user font-semibold text-3xl ml-2 text-white ">
-                5 days
+                {validSSL}
               </span>
             </div>
           </div>
