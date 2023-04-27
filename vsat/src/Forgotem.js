e8cd008d3e5de8933c49aa8123b8d7a4e0aaa1b4
Abhinav-22 <abhinavanil9@gmail.com>
2023-04-27 14:30:44 +0530

    post cors error pending

--- vsat/src/Forgotem.js
+++ vsat/src/Forgotem.js
@@ -2,15 +2,14 @@ import React from "react";
 import logo from "./img/transparent.svg";
 import { Link, useNavigate } from "react-router-dom";
 
-
 const Forgotem = () => {
   return (
     <div>
-      <div class="grid  h-screen">
-        <div class="form-pane bg-gray-900">
-          <nav class="relative w-full flex flex-wrap items-center justify-between m-0">
-            <div class="container-fluid w-full flex flex-wrap items-center justify-between">
-              <div class="container-fluid">
+      <div className="grid  h-screen">
+        <div className="form-pane bg-gray-900">
+          <nav className="relative w-full flex flex-wrap items-center justify-between m-0">
+            <div className="container-fluid w-full flex flex-wrap items-center justify-between">
+              <div className="container-fluid">
                 <Link to="/">
                   {" "}
                   <img
@@ -22,15 +21,15 @@ const Forgotem = () => {
               </div>
             </div>
           </nav>
-          <div class="mx-auto mt-32 w-full max-w-md p-4 rounded-lg shadow-md sm:p-6 md:p-8 bg-gray-800 border-gray-700 ">
-            <form class="space-y-6" action="#">
-              <h5 class="text-xl font-medium text-white">
+          <div className="mx-auto mt-32 w-full max-w-md p-4 rounded-lg shadow-md sm:p-6 md:p-8 bg-gray-800 border-gray-700 ">
+            <form className="space-y-6" action="#">
+              <h5 className="text-xl font-medium text-white">
                 Recover your VSAT account
               </h5>
               <div>
                 <label
                   for="email"
-                  class="block mb-2 text-sm font-medium text-white"
+                  className="block mb-2 text-sm font-medium text-white"
                 >
                   Enter your email
                 </label>
@@ -38,7 +37,7 @@ const Forgotem = () => {
                   type="email"
                   name="email"
                   id="email"
-                  class="text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+                  className="text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
                   placeholder="name@company.com"
                   required
                 />
@@ -46,7 +45,7 @@ const Forgotem = () => {
 
               <button
                 type="submit"
-                class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Recover account
               </button>
--- vsat/src/Individual.js
+++ vsat/src/Individual.js
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 function Induvidual() {
   return (
     <>
-      <div class="body bg-gray-900 min-h-screen">
-        <nav class="relative w-full flex flex-wrap items-center justify-between m-0">
-          <div class="container-fluid w-full flex flex-wrap items-center justify-between">
-            <div class="container-fluid">
+      <div className="body bg-gray-900 min-h-screen">
+        <nav className="relative w-full flex flex-wrap items-center justify-between m-0">
+          <div className="container-fluid w-full flex flex-wrap items-center justify-between">
+            <div className="container-fluid">
               <Link to="/">
                 {" "}
                 <img src={logo} className="h-14 mr-3 sm:h-14" alt="VSAT Logo" />
@@ -16,26 +16,26 @@ function Induvidual() {
             </div>
           </div>
         </nav>
-        <div class="container mx-auto">
-          <div class="textbar">
-            <h3 class="mx-auto my-auto mb-3 pt-4 ml-7 mt-3 text-4xl md:text-4xl xl:text-4xl font-semibold tracking-tight text-white flex flex-col items-center justify-center">
+        <div className="container mx-auto">
+          <div className="textbar">
+            <h3 className="mx-auto my-auto mb-3 pt-4 ml-7 mt-3 text-4xl md:text-4xl xl:text-4xl font-semibold tracking-tight text-white flex flex-col items-center justify-center">
               Individual's Security Platform
             </h3>
           </div>
-          <div class="flex search-bar w-full align-middle items-center justify-center">
-            <div class="w-4/6 ">
+          <div className="flex search-bar w-full align-middle items-center justify-center">
+            <div className="w-4/6 ">
               <form>
                 <label
                   for="default-search"
-                  class="mb-2 text-sm font-medium sr-only text-white"
+                  className="mb-2 text-sm font-medium sr-only text-white"
                 >
                   Search
                 </label>
-                <div class="relative">
-                  <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <div className="relative">
+                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg
                       aria-hidden="true"
-                      class="w-5 h-5 text-gray-400"
+                      className="w-5 h-5 text-gray-400"
                       fill="none"
                       stroke="currentColor"
                       viewBox="0 0 24 24"
@@ -52,13 +52,13 @@ function Induvidual() {
                   <input
                     type="search"
                     id="default-search"
-                    class="block w-full p-4 pl-10 text-sm  border  rounded-lg   bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                    className="block w-full p-4 pl-10 text-sm  border  rounded-lg   bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                     placeholder="Enter the URL to be scanned..."
                     required
                   />
                   <button
                     type="submit"
-                    class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   >
                     Search
                   </button>
@@ -66,75 +66,75 @@ function Induvidual() {
               </form>
             </div>
           </div>
-          <div class="border-2 border-slate-700 rounded-lg">
-            <div class="grid grid-cols-2 gap-5">
-              <div class="  rounded-lg reportinfo bg-gray-800 h-auto ">
-                <p class="ml-3  py-2 text-3xl text-white font-medium mb-4">
+          <div className="border-2 border-slate-700 rounded-lg">
+            <div className="grid grid-cols-2 gap-5">
+              <div className="  rounded-lg reportinfo bg-gray-800 h-auto ">
+                <p className="ml-3  py-2 text-3xl text-white font-medium mb-4">
                   Security status
                   <br />
                 </p>
-                <span class="ml-3 user font-normal text-sm text-gray-400">
+                <span className="ml-3 user font-normal text-sm text-gray-400">
                   Determine the websites's level of security by analyzing VSAT
                   security score
                 </span>
-                <hr class=" h-px my-1 w-full  border-0 bg-gray-700" />
+                <hr className=" h-px my-1 w-full  border-0 bg-gray-700" />
 
-                <h3 class="desctext text-lg font-medium text-white ml-3 mt-3">
+                <h3 className="desctext text-lg font-medium text-white ml-3 mt-3">
                   Domain status:
                 </h3>
-                <hr class="w-4/6 h-1 mx-auto   border-0 rounded md:my-10 bg-gray-700" />
+                <hr className="w-4/6 h-1 mx-auto   border-0 rounded md:my-10 bg-gray-700" />
 
-                <h3 class="desctext text-lg font-medium text-white ml-3">
+                <h3 className="desctext text-lg font-medium text-white ml-3">
                   SSL status:
                 </h3>
-                <hr class="w-4/6 h-1 mx-auto   border-0 rounded md:my-10 bg-gray-700" />
+                <hr className="w-4/6 h-1 mx-auto   border-0 rounded md:my-10 bg-gray-700" />
 
-                <h3 class="desctext text-lg font-medium text-white ml-3">
+                <h3 className="desctext text-lg font-medium text-white ml-3">
                   Phishtank status:
                 </h3>
-                <hr class="w-4/6 h-1 mx-auto   border-0 rounded md:my-10 bg-gray-700" />
+                <hr className="w-4/6 h-1 mx-auto   border-0 rounded md:my-10 bg-gray-700" />
 
-                <h3 class="desctext text-lg font-medium text-white ml-3">
+                <h3 className="desctext text-lg font-medium text-white ml-3">
                   Norton Safeweb status:
                 </h3>
 
-                <hr class="w-4/6 h-1 mx-auto   border-0 rounded md:my-10 bg-gray-700" />
+                <hr className="w-4/6 h-1 mx-auto   border-0 rounded md:my-10 bg-gray-700" />
 
-                <h3 class="desctext text-lg font-medium text-white ml-3">
+                <h3 className="desctext text-lg font-medium text-white ml-3">
                   HSTS Header status:
                 </h3>
-                <hr class="w-4/6 h-1 mx-auto   border-0 rounded md:my-10 bg-gray-700" />
+                <hr className="w-4/6 h-1 mx-auto   border-0 rounded md:my-10 bg-gray-700" />
               </div>
-              <div class=" rounded-lg scoreinfo  bg-gray-800 h-auto  ">
-                <p class="ml-3  py-2 text-3xl text-white font-medium mb-4">
+              <div className=" rounded-lg scoreinfo  bg-gray-800 h-auto  ">
+                <p className="ml-3  py-2 text-3xl text-white font-medium mb-4">
                   VSAT security score
                   <br />
                 </p>
-                <span class="ml-3 user font-normal text-sm text-gray-400">
+                <span className="ml-3 user font-normal text-sm text-gray-400">
                   Displays the security score of the website you have requested.
                 </span>
-                <hr class=" h-px my-1 w-full  border-0 bg-gray-700" />
+                <hr className=" h-px my-1 w-full  border-0 bg-gray-700" />
                 <br />
-                <div class="score bg-blue-800 h-40 w-40 rounded-full mx-auto my-auto flex flex-col items-center justify-center drop-shadow-2xl shadow-lg">
-                  <p class="ml-3  py-2 text-4xl font-black text-white  mb-4 flex flex-col items-center justify-center ">
+                <div className="score bg-blue-800 h-40 w-40 rounded-full mx-auto my-auto flex flex-col items-center justify-center drop-shadow-2xl shadow-lg">
+                  <p className="ml-3  py-2 text-4xl font-black text-white  mb-4 flex flex-col items-center justify-center ">
                     3/10
                   </p>
                 </div>
                 <br />
-                <p class="ml-3  py-2 text-2xl text-white font-light mb-4 flex flex-col items-center justify-center">
+                <p className="ml-3  py-2 text-2xl text-white font-light mb-4 flex flex-col items-center justify-center">
                   Security Index
                   <br />
                 </p>
-                <div class="indexcards w-96 h-auto border border-slate-600 rounded-xl mx-auto my-auto py-3 flex flex-col items-start  justify-center drop-shadow-2xl ">
-                  <span class=" text-md font-medium  rounded w-2/6 h-10 bg-red-900 text-red-300  flex flex-col items-center justify-center ml-3">
+                <div className="indexcards w-96 h-auto border border-slate-600 rounded-xl mx-auto my-auto py-3 flex flex-col items-start  justify-center drop-shadow-2xl ">
+                  <span className=" text-md font-medium  rounded w-2/6 h-10 bg-red-900 text-red-300  flex flex-col items-center justify-center ml-3">
                     &nbsp;RISK&nbsp;
                   </span>
                   <br />
-                  <span class=" text-md font-medium w-2/6 h-10   rounded bg-yellow-300 text-yellow-900  flex flex-col items-center justify-center ml-3">
+                  <span className=" text-md font-medium w-2/6 h-10   rounded bg-yellow-300 text-yellow-900  flex flex-col items-center justify-center ml-3">
                     &nbsp;MODERATE&nbsp;
                   </span>
                   <br />
-                  <span class="text-md font-medium w-2/6   h-10 rounded bg-green-900 text-green-300  flex flex-col items-center justify-center ml-3">
+                  <span className="text-md font-medium w-2/6   h-10 rounded bg-green-900 text-green-300  flex flex-col items-center justify-center ml-3">
                     &nbsp;SAFE&nbsp;
                   </span>
                 </div>{" "}
