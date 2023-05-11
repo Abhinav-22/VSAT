719d00d2f5455899f6e98bfc09582bd09994d8d4
Abhinav-22 <abhinavanil9@gmail.com>
2023-05-11 21:17:24 +0530

    integrated data leak scan partially

--- vsat/src/Data.js
+++ vsat/src/Data.js
@@ -9,9 +9,22 @@ const Data = () => {
   const [authName, setAuthName] = useState("");
   const [authEmail, setAuthEmail] = useState("");
   const [authCompany, setAuthCompany] = useState("");
+  const [dataleak, setDataleak] = useState("Loading...");
 
   useEffect(() => {
     console.log("workingggg");
+
+    fetch("/dataleak")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        console.log(data.DataLeak);
+        if (data.DataLeak == false) {
+          setDataleak("No Breach found");
+        } else {
+          setDataleak("Breach Found !!!");
+        }
+      });
 
     const fetchDetails = async () => {
       // await delay(1000);
@@ -266,110 +279,84 @@ const Data = () => {
           <p className="mt-3 ml-7  py-2 text-xl text-white font-light mb-4"></p>
 
           <div className="grid ml-7 grid-cols-4 justify-center">
-            
-          <div className="web h-20 w-64 drop-shadow-lg bg-gradient-to-r from-gradbl1 to-gradbl2 shadow shadow-slate-700 rounded-xl hover:bg-gray-900">
-          
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="dta float-right mt-5 mr-2 text-white bi bi-shield-exclamation" viewBox="0 0 16 16">
-                  <path d="M5.338 1.59a61.44 61.44 0 0 0-2.837.856.481.481 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.725 10.725 0 0 0 2.287 2.233c.346.244.652.42.893.533.12.057.218.095.293.118a.55.55 0 0 0 .101.025.615.615 0 0 0 .1-.025c.076-.023.174-.061.294-.118.24-.113.547-.29.893-.533a10.726 10.726 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.775 11.775 0 0 1-2.517 2.453 7.159 7.159 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.158 7.158 0 0 1-1.048-.625 11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 62.456 62.456 0 0 1 5.072.56z"/>
-                  <path d="M7.001 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.553.553 0 0 1-1.1 0L7.1 4.995z"/>
-                </svg>
-            <span className="user font-semibold text-lg ml-2 text-white">
-            Data leak status
-            </span>
-            <br />
-            <span className="user font-regular text-md ml-2 text-white">
-
-            </span>
-          </div>
-        
+            <div className="web h-20 w-64 drop-shadow-lg bg-gradient-to-r from-gradbl1 to-gradbl2 shadow shadow-slate-700 rounded-xl hover:bg-gray-900">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="40"
+                height="40"
+                fill="currentColor"
+                class="dta float-right mt-5 mr-2 text-white bi bi-shield-exclamation"
+                viewBox="0 0 16 16"
+              >
+                <path d="M5.338 1.59a61.44 61.44 0 0 0-2.837.856.481.481 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.725 10.725 0 0 0 2.287 2.233c.346.244.652.42.893.533.12.057.218.095.293.118a.55.55 0 0 0 .101.025.615.615 0 0 0 .1-.025c.076-.023.174-.061.294-.118.24-.113.547-.29.893-.533a10.726 10.726 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.775 11.775 0 0 1-2.517 2.453 7.159 7.159 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.158 7.158 0 0 1-1.048-.625 11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 62.456 62.456 0 0 1 5.072.56z" />
+                <path d="M7.001 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.553.553 0 0 1-1.1 0L7.1 4.995z" />
+              </svg>
+              <span className="user font-semibold text-lg ml-2 text-white">
+                Data leak status
+              </span>
+              <br />
+              <span className="user font-regular text-md ml-2 pt-4 text-white">
+                {dataleak}
+              </span>
+            </div>
           </div>
           <p className=" ml-7 mt-5  text-lg text-white font-medium ">
             Data breach information
-            
-            </p>
+          </p>
           <div className="scan grid grid-cols-3 gap-2">
-          
-          <div class="col-span-2 statuscard overflow-x-auto mt-3 ml-7 w-3/4 rounded-xl   max-h-screen overflow-y-auto  ">
-    <table class="w-full text-sm text-left rounded-lg text-gray-500 dark:text-gray-400 ">
-        <thead class=" text-xs text-gray-700 uppercase bg-fieldbg dark:text-gray-400 overflow-y-auto">
-            <tr>
-                <th scope="col" class="p-4">
-                    <div class="flex items-center">
-                        
-                    </div>
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    data leak information
-                </th>
-                
-               
-            </tr>
-        </thead>
-       
-        <tbody>
-        
-            <tr class="  bg-secondbg  ">
-                <td class="w-4 p-4">
-                    <div class="flex items-center">
-                       
-                    </div>
-                </td>
-               
-                <td class="px-6 py-4">
-              
-                </td>
-                
-            
-            </tr>
-            
-            
-        </tbody>
-    </table>
-    </div>
-    </div>
-    <p className=" ml-7 mt-5  text-lg text-white font-medium ">
+            <div class="col-span-2 statuscard overflow-x-auto mt-3 ml-7 w-3/4 rounded-xl   max-h-screen overflow-y-auto  ">
+              <table class="w-full text-sm text-left rounded-lg text-gray-500 dark:text-gray-400 ">
+                <thead class=" text-xs text-gray-700 uppercase bg-fieldbg dark:text-gray-400 overflow-y-auto">
+                  <tr>
+                    <th scope="col" class="p-4">
+                      <div class="flex items-center"></div>
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      data leak information
+                    </th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr class="  bg-secondbg  ">
+                    <td class="w-4 p-4">
+                      <div class="flex items-center"></div>
+                    </td>
+
+                    <td class="px-6 py-4"></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <p className=" ml-7 mt-5  text-lg text-white font-medium ">
             Privacy policy
-            
-            </p>
+          </p>
           <div className="scan grid grid-cols-3 gap-2">
-          
-          <div class="col-span-2 statuscard overflow-x-auto mt-3 ml-7 w-3/4 rounded-xl   max-h-screen overflow-y-auto  ">
-    <table class="w-full text-sm text-left rounded-lg text-gray-500 dark:text-gray-400 ">
-        <thead class=" text-xs text-gray-700 uppercase bg-fieldbg dark:text-gray-400 overflow-y-auto">
-            <tr>
-                <th scope="col" class="p-4">
-                    <div class="flex items-center">
-                        
-                    </div>
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    privacy policy status
-                </th>
-                
-               
-            </tr>
-        </thead>
-       
-        <tbody>
-        
-            <tr class="  bg-secondbg  ">
-                <td class="w-4 p-4">
-                    <div class="flex items-center">
-                       
-                    </div>
-                </td>
-               
-                <td class="px-6 py-4">
-              
-                </td>
-                
-            
-            </tr>
-            
-            
-        </tbody>
-    </table>
-    </div>
+            <div class="col-span-2 statuscard overflow-x-auto mt-3 ml-7 w-3/4 rounded-xl   max-h-screen overflow-y-auto  ">
+              <table class="w-full text-sm text-left rounded-lg text-gray-500 dark:text-gray-400 ">
+                <thead class=" text-xs text-gray-700 uppercase bg-fieldbg dark:text-gray-400 overflow-y-auto">
+                  <tr>
+                    <th scope="col" class="p-4">
+                      <div class="flex items-center"></div>
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      privacy policy status
+                    </th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr class="  bg-secondbg  ">
+                    <td class="w-4 p-4">
+                      <div class="flex items-center"></div>
+                    </td>
+
+                    <td class="px-6 py-4"></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
