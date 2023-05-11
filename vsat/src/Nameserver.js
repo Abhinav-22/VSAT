34d93b21acd749cf50b2d06caf8e356427a834f5
Abhinav-22 <abhinavanil9@gmail.com>
2023-05-12 00:18:04 +0530

    minor positioning

--- vsat/src/Nameserver.js
+++ vsat/src/Nameserver.js
@@ -1,32 +1,26 @@
 import React, { useState, useEffect } from "react";
 
+const Nameserver = () => {
+  const [server, setServer] = useState([]);
 
+  useEffect(() => {
+    fetch("/whoislookup")
+      .then((res) => res.json())
+      .then((data) => {
+        setServer(data.Whoisinfo.name_servers);
+        console.log(data);
+      });
+  }, []);
+  const n_server = server.map((str, index) => (
+    <p key={index}>
+      {index + 1}. {str}
+    </p>
+  ));
 
-
-const Nameserver=()=>{
-    const [server, setServer] = useState([]);
-
-    useEffect(() => {
-        fetch("/whoislookup")
-          .then((res) => res.json())
-          .then((data) => {
-            setServer(data.Whoisinfo.name_servers);
-            console.log(data);
-          });
-   
-        }, []);
-      const n_server = server.map((str, index) => (
-        <p key={index}>
-          {index + 1}. {str}
-        </p>
-      ));
-
-    
   return (
-    <p className="px-6 py-4 font-large text-gray-900 whitespace-nowrap dark:text-white">
+    <p className=" py-4 font-large text-gray-900 whitespace-nowrap dark:text-white">
       {n_server}
     </p>
   );
 };
-    export default  Nameserver; 
-
+export default Nameserver;
