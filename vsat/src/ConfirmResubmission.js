16913bcdeabe3b4669d1bd141babfbfd1d5a085a
dijodaiju7 <dijodaiju7@gmail.com>
2023-05-13 08:44:54 +0530

    toast for reset pwd

--- vsat/src/ConfirmResubmission.js
+++ vsat/src/ConfirmResubmission.js
@@ -22,7 +22,7 @@ const ConfirmResubmission = () => {
             </div>
           </nav>
           <section className="bg-secondbg">
-            <div className=" mx-auto max-w-screen-xl  ">
+            <div className=" mx-auto max-w-screen-md  ">
               <div className="mx-auto max-w-screen-sm text-center">
               <img
             className="w-full h-full   mx-auto  my-auto  flex flex-col items-center justify-center drop-shadow-xl opacity-80"
@@ -33,7 +33,7 @@ const ConfirmResubmission = () => {
                       <p className="mb-4 text-lg font-light text-gray-400">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
                 <Link
                   to="/"
-                  className="inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
+                  className="inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 "
                 >
                   Back to Homepage
                 </Link>
