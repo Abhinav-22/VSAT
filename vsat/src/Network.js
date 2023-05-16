3cbe896469c26861324b843a8c09f1f063364ff1
dijodaiju <dijodaijuc@gmail.com>
2023-05-16 14:51:48 +0530

    added files

--- vsat/src/Network.js
+++ vsat/src/Network.js
@@ -289,12 +289,12 @@ const Network = () => {
             </span>
           </div>
           <button>
-            <div className="web h-20 w-24  bg-fieldbg border-2 border-gradbl1 drop-shadow-xl rounded-xl hover:bg-gray-900 cursor-pointer">
+            <div className="web h-20 w-24  bg-gradbl2 border-2 border-gradbl1 drop-shadow-xl rounded-xl hover:bg-gray-900 cursor-pointer">
            
               <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class=" mx-auto my-auto bi bi-activity text-white mt-3" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z"/>
 </svg>
-              <p className="user font-regular text-md ml-2 text-center mr-2 text-white">
+              <p className="user font-semibold text-md ml-2 text-center mr-2 text-white">
                 Scan now
               </p><br></br>
              
